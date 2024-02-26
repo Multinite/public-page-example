cceb28dba3aa6b72449c295dfb22b7c6198ab4fb
Multinite <founders.multinite@gmail.com>
2024-02-27 08:58:02 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,23 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     console.log("Hello world from another site!");
-    console.log(document.cookie);
+    console.log();
     alert("test alert");
+
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (
+          !["https://multinite.com", "http://localhost:3000"].includes(
+            event.origin
+          )
+        )
+          return;
+
+        console.log("message received", event);
+      },
+      false
+    );
   }, []);
 
   return (
