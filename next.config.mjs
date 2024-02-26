c8dc0d352d95c143499ef1593cdf9fb2d3631bfb
Multinite <founders.multinite@gmail.com>
2024-02-26 21:41:10 +1000

    Updated headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "allow",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
