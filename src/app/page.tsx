28bd55bc6c4ef0929289d8e0d8866eed1b51672a
Multinite <founders.multinite@gmail.com>
2024-02-26 22:27:10 +1000

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-"use client"
+"use client";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
     console.log("Hello world from another site!");
     console.log(document.cookie);
+    alert("test alert");
   }, []);
 
-  return <div className="text-primary bg-background">hello world!</div>;
+  return (
+    <div className="text-primary bg-background">
+      hello world! - testing alert
+    </div>
+  );
 }
